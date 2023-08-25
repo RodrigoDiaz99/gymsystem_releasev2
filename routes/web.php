17b01ed7fe12835cc7b4abfdbbd85acba3df4ff5
Rodrigo Diaz Serviran <rodrigo.diaz@insejupy.gob.mx>
2023-08-25 13:13:14 -0600

    estilos login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['register'=>false,'login'=>false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('login',[LoginController::class,'login'])->name('login');
+Route::get('login', function () {
+    abort(404, 'Pagina No encontrada'); // Esto mostrará un error 403 con el mensaje 'Acceso prohibido'
+});
+Route::get('/inicio', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
