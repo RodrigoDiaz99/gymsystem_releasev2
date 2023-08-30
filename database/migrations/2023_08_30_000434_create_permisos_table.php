137b29a53f5c158ae869f3cb9cbbd126187e8e37
Kenn <kennayala@outlook.com>
2023-08-29 20:59:54 -0600

    Primeros avances roles y permisos

---
+++ database/migrations/2023_08_30_000434_create_permisos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermisosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("modulo_id");
+            $table->string("nombre");
+            $table->string("clave");
+            $table->string("descripcion");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permisos');
+    }
+}
